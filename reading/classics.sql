9199b8f7afa17d4e0f2f10e502ac5cee165078f7
Deepanjal Gupta <35405673+bakabhai993@users.noreply.github.com>
2021-11-04 00:28:05 +0530

    Update classics.sql

--- reading/classics.sql
+++ reading/classics.sql
@@ -1,1 +1,12 @@
+/*
 
+1. Notes from Underground - By Fyodor Dostoevsky
+2. Crime and Punishment - By Fyodor Dostoyevsky
+3. The Idiot - By Fyodor Dostoyevsky
+4. Demons - By Fyodor Dostoevsky
+5. The Brothers Karamazov - By Fyodor Dostoyevsky
+6. Nineteen Eighty-four - By George Orwell
+7. Anna Karenina - By Leo Tolstoy
+8. War and Peace - By Leo Tolstoy
+
+*/
